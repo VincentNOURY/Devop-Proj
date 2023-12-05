509bf179fcc8dd3c77d7c25e756da8bd5389a84d
OVilmot <oscar.vilmot@gmail.com>
2023-12-05 12:43:23 +0100

    Update Docker image repository URL

--- build_image.sh
+++ build_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t webserver .
+docker build -t 192.168.0.78:8081/library/webserver .
 docker push 192.168.0.78:8081/library/webserver:latest 
 microk8s kubectl delete -f k8s2.yaml
 microk8s kubectl apply -f k8s2.yaml
