b9a43ec63875d11e0a32863318f803708058c05f
OVilmot <oscar.vilmot@gmail.com>
2023-12-05 13:29:12 +0100

    Update app.py and k8s2.yaml

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 import mysql.connector
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
         cnx = mysql.connector.connect(user='root', password='root',
-                                                                    host='mysql',
+                                                                    host=os.environ['MYSQL_HOST'],
                                                                     database='users')
         cursor = cnx.cursor()
         query = ("SELECT username, email FROM users")
