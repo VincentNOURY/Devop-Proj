b9c5cbb02c08e9af0f656ef188e59b38152c79c5
OVilmot <oscar.vilmot@gmail.com>
2023-12-05 12:36:41 +0100

    Update image and push to registry

--- build_image.sh
+++ build_image.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 docker build -t webserver .
-docker save webserver -o webserver.tar
-microk8s ctr image import webserver.tar
+docker push 192.168.0.78:8081/library/webserver:latest 
 microk8s kubectl delete -f k8s2.yaml
 microk8s kubectl apply -f k8s2.yaml
