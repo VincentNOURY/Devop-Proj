78d13c4b92b992fc3d5f19590304266c64f02dd4
Oscar <oscar.vilmot@gmail.com>
2023-12-11 13:20:43 +0100

    Fix Docker Compose command

--- build_image.sh
+++ build_image.sh
@@ -6,6 +6,6 @@ docker push {IP}:8081/library/webserver:latest
 # sudo touch /var/snap/microk8s/current/args/certs.d/{IP}:8081/hosts.toml
 # echo "server = \"http://{IP}:8081\" \n[host.\"http://{IP}:8081\"]\ncapabilities = [\"pull\", \"resolve\"]" | tee /var/snap/microk8s/current/args/certs.d/{IP}:8081/hosts.toml
 # sudo microk8s stop && sudo microk8s start
-sudo docker-compose up -f harbor/docker-compose.yml -d
+# sudo docker-compose up -f harbor/docker-compose.yml -d
 microk8s kubectl delete -f k8s.yaml
 microk8s kubectl apply -f k8s.yaml
