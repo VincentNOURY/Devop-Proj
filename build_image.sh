b382488b5c3f97ae5e5dc1540bec161c2a7874c8
Oscar <oscar.vilmot@gmail.com>
2023-12-11 13:19:39 +0100

    Update build_image.sh script with dynamic IP address

--- build_image.sh
+++ build_image.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-docker build -t 192.168.0.78:8081/library/webserver .
-docker push 192.168.0.78:8081/library/webserver:latest
-# sudo mkdir -p /var/snap/microk8s/current/args/certs.d/192.168.0.78:8081
-# sudo touch /var/snap/microk8s/current/args/certs.d/192.168.0.78:8081/hosts.toml
-# echo "server = \"http://192.168.0.78:8081\" \n[host.\"http://192.168.0.78:8081\"]\ncapabilities = [\"pull\", \"resolve\"]" | tee /var/snap/microk8s/current/args/certs.d/192.168.0.78:8081/hosts.toml
+IP="$(ip addr show enp0s3  | awk '$1 == "inet" { print $2 }' | cut -d/ -f1)"
+docker build -t {IP}:8081/library/webserver .
+docker push {IP}:8081/library/webserver:latest
+# sudo mkdir -p /var/snap/microk8s/current/args/certs.d/{IP}:8081
+# sudo touch /var/snap/microk8s/current/args/certs.d/{IP}:8081/hosts.toml
+# echo "server = \"http://{IP}:8081\" \n[host.\"http://{IP}:8081\"]\ncapabilities = [\"pull\", \"resolve\"]" | tee /var/snap/microk8s/current/args/certs.d/{IP}:8081/hosts.toml
 # sudo microk8s stop && sudo microk8s start
 sudo docker-compose up -f harbor/docker-compose.yml -d
 microk8s kubectl delete -f k8s.yaml
