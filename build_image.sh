e11d63213aa2aad1f7621ef166d38a486e9270d2
Oscar <oscar.vilmot@gmail.com>
2023-12-11 14:47:23 +0100

    Update build_image.sh script

--- build_image.sh
+++ build_image.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 export IP="$(ip addr show ens160  | awk '$1 == "inet" { print $2 }' | cut -d/ -f1)"
-docker build -t $IP:8081/library/webserver .
-docker push $IP:8081/library/webserver:latest
 # vérifie si le dossier harbor existe
 if [ ! -d "harbor" ]; then
     wget https://github.com/goharbor/harbor/releases/download/v2.10.0-rc1/harbor-offline-installer-v2.10.0-rc1.tgz
@@ -15,5 +13,9 @@ fi
 # echo "server = \"http://$IP:8081\" \n[host.\"http://$IP:8081\"]\ncapabilities = [\"pull\", \"resolve\"]" | tee /var/snap/microk8s/current/args/certs.d/$IP:8081/hosts.toml
 # sudo microk8s stop && sudo microk8s start
 sudo docker-compose up -f harbor/docker-compose.yml -d
+docker login $IP:8081
+docker build -t $IP:8081/library/webserver .
+docker push $IP:8081/library/webserver:latest
+
 microk8s kubectl delete -f k8s.yaml
 microk8s kubectl apply -f k8s.yaml
