8c6f908317e212117ab8490bea8218050a6456dc
Oscar <oscar.vilmot@gmail.com>
2023-12-11 14:51:22 +0100

    Update docker-compose command in build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -12,8 +12,9 @@ fi
 # sudo touch /var/snap/microk8s/current/args/certs.d/$IP:8081/hosts.toml
 # echo "server = \"http://$IP:8081\" \n[host.\"http://$IP:8081\"]\ncapabilities = [\"pull\", \"resolve\"]" | tee /var/snap/microk8s/current/args/certs.d/$IP:8081/hosts.toml
 # sudo microk8s stop && sudo microk8s start
-docker-compose up -f ./harbor/docker-compose.yml -d
-docker login $IP:8081
+docker-compose -f ./harbor/docker-compose.yml up -d
+
+#docker login $IP:8081
 docker build -t $IP:8081/library/webserver .
 docker push $IP:8081/library/webserver:latest
 
