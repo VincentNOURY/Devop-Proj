737ef5a198326b922d8a761d9d2770fa9392a2c6
Oscar <oscar.vilmot@gmail.com>
2023-12-11 10:53:59 +0100

    Update Docker image and Kubernetes configuration

--- build_image.sh
+++ build_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t 192.168.0.80:8081/library/webserver .
-docker push 192.168.0.80:8081/library/webserver:latest 
+docker build -t 192.168.0.78:8081/library/webserver .
+docker push 192.168.0.78:8081/library/webserver:latest 
 microk8s kubectl delete -f k8s.yaml
 microk8s kubectl apply -f k8s.yaml
